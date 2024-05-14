fc714dd0d2e185d9b23342aef16f264270c4d27e
aibolkairoldayev <ak96.kz@gmail.com>
2024-05-14 23:54:04 +0500

    data render

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
+interface User {
+  id: number;
+  name: string;
+  surname: string;
+  email: string;
+  skills: string[];
+}
+
+const App: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    // Загрузка данных с сервера при монтировании компонента
+    axios.get<User[]>('https://8306efc93b20a953.mokky.dev/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
+  const handleDelete = (id: number) => {
+    // Логика удаления пользователя
+    console.log('Deleting user with id', id);
+  };
+
+  const handleEdit = (id: number) => {
+    // Логика редактирования пользователя
+    console.log('Editing user with id', id);
+  };
+  console.log(users)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Список пользователей</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Email</th>
+            <th>Навыки</th>
+            <th>Действия</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.surname}</td>
+              <td>{user.email}</td>
+              <td>{user.skills ? user.skills.join(', ') : ''}</td>
+              <td>
+                <button onClick={() => handleEdit(user.id)}>Редактировать</button>
+                <button onClick={() => handleDelete(user.id)}>Удалить</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
